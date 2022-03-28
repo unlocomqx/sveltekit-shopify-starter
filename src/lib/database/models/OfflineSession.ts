9c7367cab83626dcd369fd8f4e636ebeb605c36b
Jonathan <j.riche64@gmail.com>
2022-03-28 12:00:51 -0400

    Added try/catch to offline session b/c it fails

--- src/lib/database/models/OfflineSession.ts
+++ src/lib/database/models/OfflineSession.ts
@@ -1,14 +1,18 @@
 import { prisma } from "$lib/database/client";
 
 export async function getOfflineToken(shop_name: string) {
-  const shop = await prisma.shop.findUnique({
-    where: {
-      name: shop_name,
-    },
-    include: {
-      offline_session: true,
-    },
-  });
+  try {
+    const shop = await prisma.shop.findUnique({
+      where: {
+        name: shop_name,
+      },
+      include: {
+        offline_session: true,
+      },
+    });
 
-  return shop?.offline_session?.accessToken;
+    return shop.offline_session.accessToken;
+  } catch (error) {
+    return null;
+  }
 }
