55796b0d7acdeae060b2454fc4c605f17b601f86
unlocomqx <amxmodx.unloco@gmail.com>
2022-03-28 19:43:55 +0100

    Fix app bridge import

--- src/lib/shopify/app-bridge.ts
+++ src/lib/shopify/app-bridge.ts
@@ -1,16 +1,19 @@
 import type { AppConfigV1, AppConfigV2, ClientApplication } from "@shopify/app-bridge"
-import createApp from "@shopify/app-bridge"
+import AppBridge from "@shopify/app-bridge"
 import { Redirect } from "@shopify/app-bridge/actions"
 
-export function initAppBridge(shop: string, host: string) {
+// @ts-ignore
+const createApp = typeof AppBridge === "function" ? AppBridge : AppBridge.default
+
+export function initAppBridge (shop: string, host: string) {
   const apiKey = import.meta.env.VITE_SHOPIFY_API_KEY
-  const redirectUri = `${import.meta.env.VITE_SHOPIFY_HOST}/auth/callback`
-  const permissionUrl = `https://${shop}/admin/oauth/authorize?client_id=${apiKey}&scope=read_products,read_content&redirect_uri=${redirectUri}`
+  const redirectUri = `${ import.meta.env.VITE_SHOPIFY_HOST }/auth/callback`
+  const permissionUrl = `https://${ shop }/admin/oauth/authorize?client_id=${ apiKey }&scope=read_products,read_content&redirect_uri=${ redirectUri }`
 
   if (host) {
     // Redirect to the app page if the app is already authorized
     createApp({
-      apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
+      apiKey       : import.meta.env.VITE_SHOPIFY_API_KEY,
       host,
       forceRedirect: true,
     })
@@ -23,7 +26,7 @@ export function initAppBridge(shop: string, host: string) {
       // change the parent's URL with Shopify App Bridge's Redirect action
       const app = createApp({
         apiKey: apiKey,
-        host: host,
+        host  : host,
       })
 
       Redirect.create(app).dispatch(Redirect.Action.REMOTE, permissionUrl)
@@ -31,7 +34,7 @@ export function initAppBridge(shop: string, host: string) {
   }
 }
 
-export function getApp(shop = null, host = null): ClientApplication<any> {
+export function getApp (shop = null, host = null): ClientApplication<any> {
   if (!shop || !host) {
     const params = new URLSearchParams(location.search)
     shop = params.get("shop")
@@ -39,8 +42,8 @@ export function getApp(shop = null, host = null): ClientApplication<any> {
   }
 
   return createApp({
-    apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
-    shopOrigin: shop,
+    apiKey       : import.meta.env.VITE_SHOPIFY_API_KEY,
+    shopOrigin   : shop,
     host,
     forceRedirect: false,
   } as AppConfigV1 & AppConfigV2)
